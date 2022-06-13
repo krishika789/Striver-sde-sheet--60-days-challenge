fc5df4a9480db5219f7ce8cb241c5bee51d6a3fc
krishika789 <68375998+krishika789@users.noreply.github.com>
2022-06-13 07:59:43 +0530

    Update stockbuy&sell.cpp

--- DAY 1/stockbuy&sell.cpp
+++ DAY 1/stockbuy&sell.cpp
@@ -1,3 +1,18 @@
 Problem Statement: You are given an array of prices where prices[i] is the price of a given stock on an ith day.
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock. Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
-  
+#include<bits/stdc++.h>
+using namespace std;
+int maxProfit(int arr[]) {
+  int maxPro = 0;
+  int minPrice = INT_MAX;
+  for (int i = 0; i < 6; i++) {
+    minPrice = min(minPrice, arr[i]);
+    maxPro = max(maxPro, arr[i] - minPrice);
+  }
+  return maxPro;
+}
+int main() {
+  int arr[] = {7,1,5,3,6,4};
+  int maxPro = maxProfit(arr);
+  cout << "Max profit is: " << maxPro << endl;
+}  
