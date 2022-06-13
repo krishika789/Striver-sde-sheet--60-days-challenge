71ca1a0c512694bd4ae2cd3285de21a250ba3922
krishika789 <68375998+krishika789@users.noreply.github.com>
2022-06-13 08:01:31 +0530

    Update nextpermuation.cpp

--- DAY 1/nextpermuation.cpp
+++ DAY 1/nextpermuation.cpp
@@ -1,2 +1,23 @@
  Given an array Arr[] of integers, rearrange the numbers of the given array into the lexicographically next greater permutation of numbers.
-   
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size(), k, l;
+    	for (k = n - 2; k >= 0; k--) {
+            if (nums[k] < nums[k + 1]) {
+                break;
+            }
+        }
+    	if (k < 0) {
+    	    reverse(nums.begin(), nums.end());
+    	} else {
+    	    for (l = n - 1; l > k; l--) {
+                if (nums[l] > nums[k]) {
+                    break;
+                }
+            } 
+    	    swap(nums[k], nums[l]);
+    	    reverse(nums.begin() + k + 1, nums.end());
+        }
+    }
+};   
