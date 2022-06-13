b5017a48dc2da99281ba8356b69b549e1dd5be77
krishika789 <68375998+krishika789@users.noreply.github.com>
2022-06-13 08:00:25 +0530

    Update setMatrixzero.cpp

--- DAY 1/setMatrixzero.cpp
+++ DAY 1/setMatrixzero.cpp
@@ -1,2 +1,26 @@
 #set matrix zero 
 # Given a matrix if an element in the matrix is 0 then you will have to set its entire column and row to 0 and then return the matrix.
+#include<bits/stdc++.h>
+using namespace std;
+void setZeroes(vector < vector < int >> & matrix) {
+  int rows = matrix.size(), cols = matrix[0].size();
+  vector < int > dummy1(rows,-1), dummy2(cols,-1);
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      if (matrix[i][j] == 0) {
+        dummy1[i] = 0;
+        dummy2[j] = 0;
+      }
+    }
+
+  }
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      if (dummy1[i] == 0 || dummy2[j]==0) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+}
+
