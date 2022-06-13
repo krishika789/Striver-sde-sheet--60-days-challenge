d73283478e78f437de389dc0407cdd5910e43b6b
krishika789 <68375998+krishika789@users.noreply.github.com>
2022-06-13 07:59:09 +0530

    Create sort0s1s2s.cpp

---
+++ DAY 1/sort0s1s2s.cpp
@@ -0,0 +1,30 @@
+Problem Statement: Given an array consisting of only 0s, 1s and 2s. Write a program to in-place sort the array without using inbuilt sort functions. ( Expected: Single pass-O(N) and constant space)
+  class Solution {
+public:
+    void sort_elements(vector<int>& nums) {
+        int lo = 0; 
+        int hi = nums.size() - 1; 
+        int mid = 0; 
+
+        while (mid <= hi) { 
+            switch (nums[mid]) { 
+
+            // If the element is 0 
+            case 0: 
+                swap(nums[lo++], nums[mid++]); 
+                break; 
+
+            // If the element is 1 . 
+            case 1: 
+                mid++; 
+                break; 
+
+            // If the element is 2 
+            case 2: 
+                swap(nums[mid], nums[hi--]); 
+                break; 
+            }
+        }
+         
+    }
+};
